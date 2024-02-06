28842b656abe2bbc881becdcb7a28a1d094918b8
sagintel <70371441+sagintel@users.noreply.github.com>
2024-02-06 14:17:21 -0800

    Add files via upload

---
+++ constants/Colors.js
@@ -0,0 +1,14 @@
+export default {
+  bg: '#1A1936',
+  bgCard: '#494863',
+  white: '#FFFFFF',
+  black: '#000',
+  green: '#50CB93',
+  orange: '#FF5A54',
+  blue: '#36D1DC',
+  calypso: '#70EDCC',
+  red: '#D11A2A',
+  placeholder: '#B6B6B6',
+  lightGray: '#B0B3B8',
+  grey: '#C4C4C4',
+};
